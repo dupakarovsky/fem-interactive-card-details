fcfe23d65450cb80f910a80df1bd3c0e6dd6b8bd
dupakarovsky <dupakarovsky@outlook.com>
2023-04-07 18:11:10 -0300

    added iife

--- main.js
+++ main.js
@@ -17,72 +17,74 @@ import {
    animateConfIn,
 } from "./src/js/animations.js";
 
-const minSizeForDesktop = "960px";
-const maxSizeForMobile = "959px";
-const mediaChangeToDesktop = window.matchMedia(`(min-width: ${minSizeForDesktop})`);
-const mediaChangeToMobile = window.matchMedia(`(max-width: ${maxSizeForMobile})`);
-const form = select("form");
+(function () {
+   const minSizeForDesktop = "960px";
+   const maxSizeForMobile = "959px";
+   const mediaChangeToDesktop = window.matchMedia(`(min-width: ${minSizeForDesktop})`);
+   const mediaChangeToMobile = window.matchMedia(`(max-width: ${maxSizeForMobile})`);
+   const form = select("form");
 
-const inputCardHolder = select("input-cardholder");
-const inputCardNumber = select("input-number");
-const inputCardMonth = select("input-month");
-const inputCardYear = select("input-year");
-const inputCardCvc = select("input-cvc");
-const cardPreview = select("card-preview");
+   const inputCardHolder = select("input-cardholder");
+   const inputCardNumber = select("input-number");
+   const inputCardMonth = select("input-month");
+   const inputCardYear = select("input-year");
+   const inputCardCvc = select("input-cvc");
+   const cardPreview = select("card-preview");
 
-form.addEventListener("input", function (e) {
-   const input = e.target.closest("input");
-   if (!input) return;
+   form.addEventListener("input", function (e) {
+      const input = e.target.closest("input");
+      if (!input) return;
 
-   switch (true) {
-      case e.target === inputCardHolder:
-         animateCardHolder(e);
-         checkForErrorBlank(e, e.target);
-         break;
-      case e.target === inputCardNumber:
-         checkForErrorBlank(e, e.target);
-         checkForErrorInvalidNumberFormat(e, e.target);
-         animateCardNumber(e);
-         break;
-      case e.target === inputCardMonth:
-         checkForErrorBlank(e, e.target);
-         checkErrorInvalidMonth(e, e.target);
-         animateCardField(e, "month");
-         break;
-      case e.target === inputCardYear:
-         checkForErrorBlank(e, e.target);
-         checkErrorInvalidYear(e, e.target);
-         animateCardField(e, "year");
-         break;
-      case e.target === inputCardCvc:
-         checkForErrorBlank(e, e.target);
-         animateCardField(e, "cvc");
-         break;
-      default:
-         "";
-   }
-});
-form.addEventListener("submit", function (e) {
-   e.preventDefault();
-   if (inputCardMonth.dataset.inError === "true") return;
-   animatedFormOut();
-   animateConfIn();
-});
+      switch (true) {
+         case e.target === inputCardHolder:
+            animateCardHolder(e);
+            checkForErrorBlank(e, e.target);
+            break;
+         case e.target === inputCardNumber:
+            checkForErrorBlank(e, e.target);
+            checkForErrorInvalidNumberFormat(e, e.target);
+            animateCardNumber(e);
+            break;
+         case e.target === inputCardMonth:
+            checkForErrorBlank(e, e.target);
+            checkErrorInvalidMonth(e, e.target);
+            animateCardField(e, "month");
+            break;
+         case e.target === inputCardYear:
+            checkForErrorBlank(e, e.target);
+            checkErrorInvalidYear(e, e.target);
+            animateCardField(e, "year");
+            break;
+         case e.target === inputCardCvc:
+            checkForErrorBlank(e, e.target);
+            animateCardField(e, "cvc");
+            break;
+         default:
+            "";
+      }
+   });
+   form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      if (inputCardMonth.dataset.inError === "true") return;
+      animatedFormOut();
+      animateConfIn();
+   });
 
-inputCardCvc.addEventListener("focusin", (e) => {
-   if (mediaChangeToMobile.matches) cardFlipMobile.play();
-});
-inputCardCvc.addEventListener("focusout", (e) => {
-   if (mediaChangeToMobile.matches) cardFlipMobile.reverse();
-});
+   inputCardCvc.addEventListener("focusin", (e) => {
+      if (mediaChangeToMobile.matches) cardFlipMobile.play();
+   });
+   inputCardCvc.addEventListener("focusout", (e) => {
+      if (mediaChangeToMobile.matches) cardFlipMobile.reverse();
+   });
 
-mediaChangeToDesktop.addEventListener("change", (e) => {
-   if (e.target.matches) {
-      removeInlineStyles(cardPreview);
-   }
-});
+   mediaChangeToDesktop.addEventListener("change", (e) => {
+      if (e.target.matches) {
+         removeInlineStyles(cardPreview);
+      }
+   });
 
-window.addEventListener("load", () => {
-   setDefaultStateValues();
-   animateCardFlipMobile();
-});
+   window.addEventListener("load", () => {
+      setDefaultStateValues();
+      animateCardFlipMobile();
+   });
+})();
